329b34abbfbc5a68724d6216e3a5430052710534
Gun <cfr2ak@gmail.com>
2021-11-28 20:09:22 +0900

    Simulating circular lattice

--- mlat/dim2/__init__.py
+++ mlat/dim2/__init__.py
@@ -2,3 +2,4 @@ from .kagome import KagomeLattice
 from .squareAB import SquareABLattice
 from .triangular import TriangularLattice
 from .hexagonal import HexagonalLattice
+from .circular import CircularLattice
